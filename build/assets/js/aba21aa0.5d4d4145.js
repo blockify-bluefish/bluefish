0095a59b566b4280747f1a7d623a66197cbcc68c
Jack (⚓⚓⚓) Sparrow <jackbereson@gmail.com>
2025-06-04 15:08:35 +0700

    fix: aa

--- build/assets/js/aba21aa0.5d4d4145.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkbluefish=self.webpackChunkbluefish||[]).push([[5742],{7093:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://seashell-app-29z2d.ondigitalocean.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/bluefish/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

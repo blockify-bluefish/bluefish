186b43190b5a6fcec7ffe62d75dcb15179559ffe
Jack (⚓⚓⚓) Sparrow <jackbereson@gmail.com>
2025-06-04 18:08:38 +0700

    fix: build

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  distDir: "dist",
+  output: "export",
 };
 
 export default withFlowbiteReact(nextConfig);
